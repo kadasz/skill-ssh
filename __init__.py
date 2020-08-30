0ec8328af11409407da120df1a8d97c2a450c97c
Karol Sz <(none)>
2020-08-30 12:37:25 +0200

    MySSHClient, fix: missing import, less connection attempts, add: port setting attribiute

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import time
+import socket
 import logging
 from typing import List, Union
 from paramiko import SSHClient, AutoAddPolicy, RSAKey
@@ -6,18 +7,25 @@ from paramiko.auth_handler import AuthenticationException, SSHException
 
 logger = logging.getLogger(__name__)
 
+class ServerError(Exception):
+    """
+    An exception class for exceptional behavior that occurs while working with servers.
+    """
+    pass
+
 class MySSHClient(object):
     """
         Client to communicate with remote servers via SSH
     """
 
-    def __init__(self, host: str, user: str , ssh_key: str) -> None:
+    def __init__(self, host: str, port: str, user: str , ssh_key: str) -> None:
         self.host = host
+        self.port = port
         self.user = user
         self.ssh_key = ssh_key
         self.client = None
         self.session = None
-        self.conn_attempts = 20
+        self.conn_attempts = 10
 
     def __exit__(self) -> None:
         self._disconnect()
@@ -34,6 +42,7 @@ class MySSHClient(object):
                     self.client.set_missing_host_key_policy(AutoAddPolicy())
                     self.client.connect(
                         self.host,
+                        self.port,
                         username=self.user,
                         key_filename=self.ssh_key,
                         look_for_keys=True,
@@ -42,7 +51,7 @@ class MySSHClient(object):
                     break
                 except (AuthenticationException,  SSHException) as e:
                     logger.error(f'Unable to authenticate with remote server: {e}')
-                    raise error
+                    raise e
                 except (socket.error, EOFError):
                     logger.error(f'Connection refused, retrying...')
                     time.sleep(tries + 1)
